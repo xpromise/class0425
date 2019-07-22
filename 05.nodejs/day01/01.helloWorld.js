c46fe58e2850cbbb306023317699eb6017880566
xiongjian <webjsforyou@163.com>
2019-07-22 09:46:32 +0800

    nodejs的基本组成

---
+++ 05.nodejs/day01/01.helloWorld.js
@@ -0,0 +1,19 @@
+/*
+  1. 运行nodejs代码
+    - 右键 run
+    - 在cmd / terminal中，来到当前文件所在目录。 运行 node 文件名
+  2. nodejs 是使用js开发的。
+    js组成：
+      DOM 没有DOM
+      BOM 包含少量的BOM
+        console
+        setTimeout
+        setInterval
+      ES 包含基本上所有的ES
+
+ */
+console.log('hello world');
+
+// console.log(document);
+// console.log(window);
+console.log(global); // 全局对象

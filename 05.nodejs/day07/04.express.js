51ce6f9759a306344790de7debad5db71f8f16d5
xiongjian <webjsforyou@163.com>
2019-07-29 16:21:20 +0800

    解决跨域方案-CORS

--- 05.nodejs/day07/04.express.js
+++ 05.nodejs/day07/04.express.js
@@ -9,9 +9,12 @@ app.use(express.urlencoded({extended: true}));
 app.get('/test', (req, res) => {
   console.log(req.query); // { username: 'jack', password: '123' }
 
+  // 允许所有地址进行跨域访问
+  res.set('Access-Control-Allow-Origin', '*');
+
   const data = {
-    name: 'jack',
-    age: 18
+    name: 'rose',
+    age: 20
   };
 
   res.send(data);
@@ -19,6 +22,30 @@ app.get('/test', (req, res) => {
 
 app.post('/test', (req, res) => {
   console.log(req.body); // { username: 'rose', password: '456' }
+
+  console.log(req.hostname);
+
+  /*
+    解决跨域方案：
+      CORS： 官方推出的解决方案。
+        特点：
+          1. 能处理所有请求（method请求方式）
+          2. 兼容性稍差（http协议来规定，老版本浏览器不认识）
+   */
+
+  const safeUrl = ['http://localhost:63342', 'http://localhost:4000', 'http://localhost:5000'];
+  const result = safeUrl.find((url) => url.includes(req.hostname));
+  console.log(result);
+
+  if (result) {
+    // 允许所有地址进行跨域访问
+    // res.set('Access-Control-Allow-Origin', '*');
+    // 允许单个地址进行跨域访问
+    res.set('Access-Control-Allow-Origin', result);
+    // res.set('Access-Control-Allow-Methods', 'GET');
+    // res.set('Access-Control-Allow-Credentials', true);
+  }
+
 
   const data = {
     name: 'jack',

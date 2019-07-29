39d7f9359ed7aa971a3f1c726bf393cf96c1960e
xiongjian <webjsforyou@163.com>
2019-07-29 17:17:50 +0800

    解决跨域方案-JSONP

--- 05.nodejs/day07/04.express.js
+++ 05.nodejs/day07/04.express.js
@@ -20,17 +20,32 @@ app.get('/test', (req, res) => {
   res.send(data);
 });
 
+
 app.post('/test', (req, res) => {
   console.log(req.body); // { username: 'rose', password: '456' }
 
   console.log(req.hostname);
 
   /*
+    同源策略：
+      1. 协议名
+      2. 域名
+      3. 端口号
+      三者必须完全一致，一旦有一个不一样，就是违背同源策略，就会产生跨域
+      结果：导致用户拿不到响应数据
+
+      ajax请求会经过同源策略的检查，所以才有跨域问题
+      而 script img link form 等HTML标签，它们不会经过同源策略的检查，就没有跨域问题
+
     解决跨域方案：
       CORS： 官方推出的解决方案。
         特点：
           1. 能处理所有请求（method请求方式）
           2. 兼容性稍差（http协议来规定，老版本浏览器不认识）
+      JSONP: 民间的解决方案
+        特点：
+          1. 只能处理GET请求
+          2. 兼容性极好
    */
 
   const safeUrl = ['http://localhost:63342', 'http://localhost:4000', 'http://localhost:5000'];
@@ -42,7 +57,7 @@ app.post('/test', (req, res) => {
     // res.set('Access-Control-Allow-Origin', '*');
     // 允许单个地址进行跨域访问
     res.set('Access-Control-Allow-Origin', result);
-    // res.set('Access-Control-Allow-Methods', 'GET');
+    // res.set('Access-Control-Allow-Methods', 'GET, POST, HEAD, PUT');
     // res.set('Access-Control-Allow-Credentials', true);
   }
 
@@ -53,6 +68,19 @@ app.post('/test', (req, res) => {
   };
 
   res.send(data);
+});
+
+app.get('/jsonp', (req, res) => {
+  // getData
+  const { callback } = req.query;
+
+  const data = {
+    name: 'tom',
+    age: 30
+  };
+
+  // 返回响应
+  res.send(`${callback}(${JSON.stringify(data)})`); //  'getData({"name": "tom", "age": 30})'
 });
 
 app.listen(3000, (err) => {
